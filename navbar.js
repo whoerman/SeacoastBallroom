d6ce62d8f011214f3ec08da2413b90e1c81dba36
Walter Hoerman <whoerman@gmail.com>
2023-01-02 14:04:37 -0500

    saving navbar

---
+++ navbar.js
@@ -0,0 +1,25 @@
+
+    let navheader = '<nav class="navbar navbar-light text-center bg-light"> \
+    <a href="https://www.seacoastballroom.com"><span class="navbar-brand text-center mb-0 h1"><i class="fas fa-home"></i>Seacoast Ballroom</em> \
+        <Dlg></Dlg> \
+      </span></a> \
+    <ul class="nav text-center"> \
+      <li class="nav-item"> \
+        <a class="nav-link" href="./index.html">Home</a> \
+      </li> \
+      <li class="nav-item"> \
+        <a class="nav-link" href="./index.html">Home</a> \
+      </li> \
+      <li class="nav-item"> \
+      <a class="nav-link" href="./index.html">Home</a> \
+      </li> \
+      <li class="nav-item"> \
+      <a class="nav-link" href="./index.html">Home</a> \
+      </li> \
+    </ul> \
+    </nav>'
+    
+    
+    
+    document.getElementById("navbardiv").innerHTML +=
+    navheader;
