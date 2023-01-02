1541b9db43a77d687e00187c7c8d0f214f19ccd5
Walter Hoerman <whoerman@gmail.com>
2023-01-02 14:15:24 -0500

    adding header image

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
 
     let navheader = '<nav class="navbar navbar-light text-center bg-light"> \
-    <a href="https://www.seacoastballroom.com"><span class="navbar-brand text-center mb-0 h1"><i class="fas fa-home"></i>Seacoast Ballroom</em> \
+    <a href="https://www.seacoastballroom.com"><span class="navbar-brand text-center mb-0 h1"><i class="fas fa-home"></i> Seacoast Ballroom</em> \
         <Dlg></Dlg> \
       </span></a> \
     <ul class="nav text-center"> \
